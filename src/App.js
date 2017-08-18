4ea5e284f9ffba16921882e25a2bb78775c911eb
Tim Humphries <thumphries5@hotmail.com>
2017-08-18 11:53:39 +0100

    again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 48092f95db461000af31599b96f209669f4ca527
 import React from 'react'; // eslint-disable-line
 import { Provider } from 'react-redux';
 import { Navigation } from 'react-native-navigation';
 import { registerScreens } from './RegisterScreens';
-<<<<<<< HEAD
 import  store  from './Redux/Store';
 import ToDoView from './modules/ToDo/ToDoView';
 import reducer from './Redux/Reducer'
@@ -18,17 +13,4 @@ Navigation.startSingleScreenApp({
 		screen: 'app.ToDoView',
 		title: 'To Do List',
 	},
-=======
-import configureStore from './store/configureStore';
-
-const store = configureStore();
-
-registerScreens(store, Provider);
-
-Navigation.startSingleScreenApp({
-	screen: {
-		screen: 'app.Home',
-		title: 'Sample App',
-	}
->>>>>>> 48092f95db461000af31599b96f209669f4ca527
 });
--- src/RegisterScreens.js
+++ src/RegisterScreens.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable import/prefer-default-export */
 import { Navigation } from 'react-native-navigation';
 import ToDoView from './modules/ToDo/ToDoViewContainer';
@@ -8,12 +7,3 @@ import { Provider } from 'react-redux';
 export function registerScreens() {
 		Navigation.registerComponent('app.ToDoView', () => ToDoView, store, Provider);
 }
-=======
-import { Navigation } from 'react-native-navigation';
-
-import Todo from './modules/Todo/Todo'
-
-export function registerScreens(){
-    Navigation.registerComponent('app.Todo', () => Todo);
-}
->>>>>>> 48092f95db461000af31599b96f209669f4ca527
